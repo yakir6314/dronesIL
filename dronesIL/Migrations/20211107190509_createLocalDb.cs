f610526eef01dfa2a7425253bd04a15cf93b419b
yakirkapitnik <48790174+yakirkapitnik@users.noreply.github.com>
2021-11-07 21:18:14 +0200

    add as local db

---
+++ dronesIL/Migrations/20211107190509_createLocalDb.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dronesIL.Migrations
+{
+    public partial class createLocalDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "sum",
+                table: "Order",
+                newName: "Sum");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Sum",
+                table: "Order",
+                newName: "sum");
+        }
+    }
+}
