895a03af74d4e78e9084f75c4b7e76ca7cb74dbb
noashafir <noashafir97@gmail.com>
2021-11-10 22:31:49 +0200

    fix issues

--- dronesIL/wwwroot/js/site.js
+++ dronesIL/wwwroot/js/site.js
@@ -3,6 +3,13 @@ var script = document.createElement('script');
 script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
+    $(window).load(function() {
+    // Animate loader off screen
+    $(".se-pre-con").fadeOut("slow");;
+});
+
+
+
 
 function goToBasket() {
     var cart;
@@ -69,3 +76,5 @@ function searchKeyPress() {
 
 
 }
+
+
