8950f18b03d4b2b3ff5526f495daa51d9619e8ec
noashafir <noashafir97@gmail.com>
2021-11-12 20:23:49 +0200

    design all the modal index  view

--- dronesIL/Models/Drone.cs
+++ dronesIL/Models/Drone.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,13 +9,21 @@ namespace dronesIL.Models
 {
     public class Drone
     {
+        [DisplayName("מספר קטלוגי")]
         [Key]
+        
         public int droneId { get; set; }
+        [DisplayName("שם דגם רחפן")]
         public string name { get; set; }
+        [DisplayName("מחיר")]
         public decimal price { get; set; }
+        [DisplayName("תאריך יצירה")]
         public DateTime createDate { get; set; }
+        [DisplayName("תאריך עדכון")]
         public DateTime LastUpdateDate { get; set; }
+        [DisplayName("תיאור רחפן")]
         public string description { get; set; }
+        [DisplayName("תמונה")]
         public string imageUrl { get; set; }
         public ICollection<DronesOrders> dronesOrders { get; set; }
 
