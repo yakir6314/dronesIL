ffb66d9a05662fc312b2ea00287895699f106d92
yakirkapitnik <48790174+yakirkapitnik@users.noreply.github.com>
2021-11-13 22:22:00 +0200

    add 3 filter

---
+++ dronesIL/Migrations/20211113193148_DroneEnt_add.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace dronesIL.Migrations
+{
+    public partial class DroneEnt_add : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "isEnterprise",
+                table: "Drone",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "isEnterprise",
+                table: "Drone");
+        }
+    }
+}
